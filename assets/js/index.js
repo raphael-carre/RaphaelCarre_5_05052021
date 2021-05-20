6de8be12fe066a21cd16aa264c48aed9c736632e
Raphaël Carré <raphaelcarre@me.com>
2021-05-20 17:46:08 +0200

    Cart class name correction in index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,9 @@
 import Router from './classes/Router'
-import Card from './classes/Cart'
+import Cart from './classes/Cart'
 
 const BASE_API_URL = 'https://ocorino.herokuapp.com/api/teddies'
 
 const router = new Router(BASE_API_URL)
 
 router.route()
-Card.headerNotification()
+Cart.headerNotification()
